20f94ae28d604687e808487d9818ce862e769ae9
Ben Ivey <benjaminivey00@gmail.com>
2020-08-22 16:17:38 -0600

    initial commit

---
+++ planner.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+
+    $(".saveBtn").on("click", function(){
+       var value = $(this).siblings('.textInput').val()
+       var time = $(this).parent().attr('id')
+       localStorage.setItem(time, value)
+       });
+
+console.log(localStorage.getItem('time1'))
+
+$ ("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
+
+function updateColor(){
+
+    let currentHour = moment().hour();
+    $('.timeBlock').each(function(){
+    var blockHour = parseInt($(this).attr('id').split('-')[1])
+    if (blockHour < currentHour) {
+        $(this).addClass('past')
+    }
+    else if (blockHour === currentHour) {
+        $(this).removeClass('past')
+        $(this).addClass('present')
+    }
+    else {
+        $(this).removeClass('past')
+        $(this).removeClass('present')
+        $(this).addClass('future')
+    }
+    })
+}
+
+$('#time-8 .textInput').val(localStorage.getItem('time-8'))
+$('#time-9 .textInput').val(localStorage.getItem('time-9'))
+$('#time-10 .textInput').val(localStorage.getItem('time-10'))
+$('#time-11 .textInput').val(localStorage.getItem('time-11'))
+$('#time-12 .textInput').val(localStorage.getItem('time-12'))
+
+updateColor();
+
+});
